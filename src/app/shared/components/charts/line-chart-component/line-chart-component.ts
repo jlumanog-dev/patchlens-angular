a0adb240dedfcd38351e48b1f952df6c5e5db286
Joshua Lumanog <lumanog.joshua05@gmail.com>
2025-11-25 22:14:24 +0800

    fix line-chart-component reusability

--- src/app/shared/components/charts/line-chart-component/line-chart-component.ts
+++ src/app/shared/components/charts/line-chart-component/line-chart-component.ts
@@ -17,7 +17,7 @@ export class LineChartComponent {
   lineChartLabel = input<string[]>([]);
 
   //inputs and signals for insights-view
-  
+
 
 
   /* definite assign operator (!) tells angular the 'chart' will get assigned later
@@ -31,10 +31,26 @@ export class LineChartComponent {
 
     switch(this.usedBy()){
       case 'hero-detail':
-          this.lineChartData.labels = [...this.lineChartLabel()];
-          this.lineChartData.datasets[0].data = [...this.pub_win_trend()];
-          this.lineChartData.datasets[1].data = [...this.pub_pick_trend()];
-          this.chart?.update();
+        this.lineChartData = {
+          labels: [...this.lineChartLabel()],   // match array length
+          datasets: [
+            {
+              data: [],
+              label: '',
+              fill: false,
+            },
+            {
+              data: [],
+              label: '',
+              fill: false,
+            }
+          ]
+        }
+        this.lineChartData.datasets[0].data = [...this.pub_win_trend()];
+        this.lineChartData.datasets[0].label = 'Win Trend!';
+        this.lineChartData.datasets[1].data = [...this.pub_pick_trend()];
+        this.lineChartData.datasets[1].label = 'Pick Trend!';
+        this.chart?.update();
         break;
       case 'insight-view':
 
@@ -47,16 +63,6 @@ export class LineChartComponent {
   lineChartData: ChartConfiguration['data'] = {
     labels: [],   // match array length
     datasets: [
-      {
-        data: [],
-        label: 'Win Trend',
-        fill: false,
-      },
-      {
-        data: [],
-        label: 'Pick Trend',
-        fill: false,
-      },
     ]
   };
 
