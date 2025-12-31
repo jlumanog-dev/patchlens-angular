65a58169dadba724afaaadb9bb4571c505568e51
Joshua Lumanog <joshualumanog.dev@gmail.com>
2025-12-31 20:38:29 +0800

    changed on environment.prod to use http

--- src/app/environment/environment.prod.ts
+++ src/app/environment/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiBaseUrl: 'https://ec2-18-143-160-41.ap-southeast-1.compute.amazonaws.com:8080',
+  apiBaseUrl: 'http://ec2-18-143-160-41.ap-southeast-1.compute.amazonaws.com:8080',
   production: true
 };
