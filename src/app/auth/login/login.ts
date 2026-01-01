7c242296a119ccde169ff6f0b31be67f22166188
Joshua Lumanog <joshualumanog.dev@gmail.com>
2026-01-01 09:08:38 +0800

    minor changes

--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -65,7 +65,7 @@ export class Login {
           this.loginFormGroup.setErrors({
             hasError: error.error.message
           })
-          this.pinInvalidMessage.set(error.error.message);
+          this.pinInvalidMessage.set("EC2 Instance currently inactive");
         }
       });
     }
